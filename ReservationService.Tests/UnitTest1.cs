b1dc392a3082b250a1cbc1f4d961dc0f58592f04
mikolajtr <mikolajtr2@wp.pl>
2015-10-25 23:27:25 +0100

    initial commit

---
+++ ReservationService.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReservationService.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ReservationService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReservationService.Global" Language="C#" %>
