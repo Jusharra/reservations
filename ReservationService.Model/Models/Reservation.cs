b4119c9155475ce451bfd8a0c6cfbde0e7cfcdbc
mikolajtr <mikolajtr2@wp.pl>
2015-10-26 01:23:33 +0100

    added personal informations to User model

--- ReservationService.Model/Models/Reservation.cs
+++ ReservationService.Model/Models/Reservation.cs
@@ -5,8 +5,13 @@ namespace ReservationService.Model.Models
     public class Reservation
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ItemId { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public string Notes { get; set; }
+
+        public User User { get; set; }
+        public Item Item { get; set; }
     }
 }
--- ReservationService.Model/Models/User.cs
+++ ReservationService.Model/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -7,6 +8,16 @@ namespace ReservationService.Model.Models
 {
     public class User : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string HomeNumber { get; set; }
+        public string FlatNumber { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
